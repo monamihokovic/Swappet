0302d5282c34205ffd0b420eef1e84427bd17f2c
Rodjowara <ivan-vjekoslav.rodak@fer.hr>
2024-10-26 19:11:42 +0200

    Added OAUth2.TODO: frontend and testing

--- backend/Swappet/src/main/java/com/swappet/Swappet/service/AuthConfig.java
+++ backend/Swappet/src/main/java/com/swappet/Swappet/service/AuthConfig.java
@@ -1,13 +1,20 @@
 package com.swappet.Swappet.service;
 
+import com.swappet.Swappet.model.User;
+import com.swappet.Swappet.repository.UserRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -17,7 +24,7 @@ import java.io.IOException;
 //spremanje u bazu i veza sa frontendom - treba otkriti kako
 
 @Configuration
-public class AuthConfig {
+public class AuthConfig extends DefaultOAuth2UserService {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -38,5 +45,23 @@ public class AuthConfig {
                 })
                 .formLogin(Customizer.withDefaults())
                 .build();
+    }
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        int id = oAuth2User.getAttribute("id");
+        String username = oAuth2User.getAttribute("username");
+        String email = oAuth2User.getAttribute("email");
+
+        // Save or update user details in the database
+        User user = new User(id, username, email);
+        userRepository.save(user);
+
+        return oAuth2User;
     }
 }
