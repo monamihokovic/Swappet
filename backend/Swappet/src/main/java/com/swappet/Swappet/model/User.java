6ea120157330adaf1f1d930c86330a0e4bbb47a7
Rodjowara <ivan-vjekoslav.rodak@fer.hr>
2024-10-26 19:14:21 +0200

    Added OAuth2. TODO: frontend and testing

--- backend/Swappet/src/main/java/com/swappet/Swappet/model/User.java
+++ backend/Swappet/src/main/java/com/swappet/Swappet/model/User.java
@@ -19,6 +19,12 @@ public class User {
     public User() {
     }
 
+    public User(int id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+
     public int getId() {
         return id;
     }
