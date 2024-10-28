87719cfe6b5daf0a91215b2f60e48935fb64248c
Rodjowara <ivan-vjekoslav.rodak@fer.hr>
2024-10-28 11:22:42 +0100

    Fixed a bug in UserServiceImpl

--- backend/Swappet/src/main/java/com/swappet/Swappet/repository/UserRepository.java
+++ backend/Swappet/src/main/java/com/swappet/Swappet/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.swappet.Swappet.repository;
 
-import org.apache.catalina.User;
+import com.swappet.Swappet.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
