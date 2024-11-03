ad7e1f2721b17e4566d400cfd7c01f951c1d41d5
monamihokovic <mona.mihokovic@fer.unizg.hr>
2024-11-03 10:43:36 +0100

    popravljanje repozitorija

--- Swappet/src/main/java/PROGI/Swappet/repository/JeUkljucenRepository.java
+++
@@ -1,9 +0,0 @@
-package PROGI.Swappet.repository;
-
-import PROGI.Swappet.model.JeUkljucen;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JeUkljucenRepository extends JpaRepository<JeUkljucen, String> {
-}
