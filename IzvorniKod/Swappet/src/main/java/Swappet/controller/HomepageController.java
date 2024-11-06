70c5cd9e5a8b7ba884988519c951290632ec714e
Mona M <monatipka@gmail.com>
2024-11-06 18:50:36 +0100

    Izmjene u kodu HomepageControllera i application.properties

--- IzvorniKod/Swappet/src/main/java/Swappet/controller/HomepageController.java
+++ IzvorniKod/Swappet/src/main/java/Swappet/controller/HomepageController.java
@@ -1,28 +1,17 @@
 package Swappet.controller;
 
-<<<<<<< Updated upstream
-import org.springframework.web.bind.annotation.GetMapping;
-=======
 import Swappet.model.Oglas;
 import Swappet.service.OglasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
->>>>>>> Stashed changes
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-<<<<<<< Updated upstream
-public class HomepageController {
-
-    @RequestMapping("/homepage")
-    public String home(){
-        return "Shadow, it's me, the DEVIL";
-=======
 @RequestMapping("/api/homepage")
 public class HomepageController {
 
@@ -36,7 +25,5 @@ public class HomepageController {
     public ResponseEntity<List<Oglas>> getOglasByCategories(@RequestBody List<Integer> categories) {
         List<Oglas> oglasi = oglasService.getOglasByCategories(categories);
         return ResponseEntity.ok(oglasi);
->>>>>>> Stashed changes
     }
-
 }
