31467dd76f8b91f9e564409dd99f7e08429bce04
gt726 <156925557+gt726@users.noreply.github.com>
2024-11-13 15:16:59 +0100

    Popravljanje bugova

--- IzvorniKod/Swappet/backend/src/test/java/Fullstack/web/app/SwappetApplicationTests.java
+++ IzvorniKod/Swappet/backend/src/test/java/Fullstack/web/app/SwappetApplicationTests.java
@@ -1,9 +1,10 @@
 package Fullstack.web.app;
 
+import Swappet.service.SwappetApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SwappetApplication.class)
 class SwappetApplicationTests {
 
 	@Test
