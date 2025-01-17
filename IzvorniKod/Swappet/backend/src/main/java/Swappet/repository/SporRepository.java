39785b8f209a35c9a4aa4285a1517098630c8f69
Mona M <monatipka@gmail.com>
2025-01-17 16:53:14 +0100

    Provjera je li korisnikov račun blokiran

--- IzvorniKod/Swappet/backend/src/main/java/Swappet/repository/SporRepository.java
+++ IzvorniKod/Swappet/backend/src/main/java/Swappet/repository/SporRepository.java
@@ -5,10 +5,13 @@ import Swappet.model.Spor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface SporRepository extends JpaRepository<Spor, Integer> {
 
     Spor findByTuzen(Korisnik korisniks);
 
+    List<Spor> findByTuzenEmail(String tuzeniEmail);
 }
