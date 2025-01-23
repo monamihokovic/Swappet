3aa616f6774d9cb14781c74d9584d0ed53cc93de
unknown <maja.blazok@fer.hr>
2025-01-23 23:34:59 +0100

    redizajn frontenda za bolji i cleaner look :D

--- IzvorniKod/Swappet/frontend/src/components/AdminReported.js
+++ IzvorniKod/Swappet/frontend/src/components/AdminReported.js
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import "../css/AdminReported.css";
 import axios from "axios";
+import Header from "./Header";
 
 const defaultProfilePic = "/defaultpfp.jpg";
 
-const AdminReported = ({ profilePic }) => {
-    const navigate = useNavigate();
+const AdminReported=()=>{ 
+    const [user, setUser] = useState(null); //inicijalizacija korisnika
+    const [reportedAccounts, setReported] = useState([]); //inicijalizacija prijavljenih računa
 
-    const [user, setUser] = useState(null);
-    const [reportedAccounts, setReported] = useState([]);
-
-    // Fetch user info
+    //dohvati informacije o korisniku
     useEffect(() => {
         axios
             .get(`${process.env.REACT_APP_BACKEND_URL}/user-info`, {
@@ -25,6 +24,7 @@ const AdminReported = ({ profilePic }) => {
             });
     }, []);
 
+    //dohvati reportane usere
     useEffect(() => {
         axios
             .get(`${process.env.REACT_APP_BACKEND_URL}/admin/guilty`, {
@@ -38,7 +38,9 @@ const AdminReported = ({ profilePic }) => {
                 console.error("Error fetching reported users: ", error);
             });
     }, []);
+    
 
+    //rukovanje 'banom'
     const handleBan = (email, action) => {
         console.log("Mail korisnika: " + email);
         axios
@@ -54,55 +56,22 @@ const AdminReported = ({ profilePic }) => {
             });
     };
 
-    return (
+    return(
         <div className="admin-page">
-            <div className="header">
-                <div className="profile">
-                    <img
-                        src={user?.picture || defaultProfilePic}
-                        alt="Profile"
-                        className="pfp"
-                        onError={(e) => {
-                            e.target.src = defaultProfilePic;
-                        }}
-                    />
-                    <div
-                        className="username"
-                        onClick={() => navigate("/advertisements")}
-                    >
-                        {user ? user.name : "Loading..."}
-                    </div>
-                </div>
-
-                <div className="logo" onClick={() => navigate("/")}>
-                    S<span id="usklicnik">!</span>
-                </div>
-            </div>
-
-            <div className="container">
-                <h2 id="oglasi">Svi prijavljeni korisnici </h2>
-
-                <div className="Useri">
-                    {reportedAccounts.length === 0 ? (
+            <Header></Header>
+            <div className="container-korisnika">
+                <div id="reported">Svi prijavljeni korisnici</div>
+                <div className="useri">
+                {reportedAccounts.length === 0 ? (
                         <div className="no-events-message">
                             Nema prijavljenih korisnika.
                         </div>
                     ) : (
                         reportedAccounts.map((user) => (
-                            <div className="Users">
-                                {user}
-                                <button
-                                    value={0}
-                                    onClick={() => handleBan(user, 1)}
-                                >
-                                    Oslobodi
-                                </button>
-                                <button
-                                    value={1}
-                                    onClick={() => handleBan(user, 0)}
-                                >
-                                    Zabrani
-                                </button>
+                            <div className="korisnik">
+                                <div id="email">Korisnik: {user}</div>
+                                <button value={1} onClick={handleBan(user, 1)} className="ban-button">Oslobodi</button>
+                                <button value={0} onClick={handleBan(user, 0)} className="ban-button">Zabrani</button>
                             </div>
                         ))
                     )}
@@ -111,5 +80,4 @@ const AdminReported = ({ profilePic }) => {
         </div>
     );
 };
-
 export default AdminReported;
